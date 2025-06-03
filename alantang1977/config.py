7f2f72120fc2a3addaf0c6a560d3c111c07db3e0
GitHub Action <action@github.com>
2025-06-03 03:35:41 +0000

    Sync https://github.com/alantang1977/X.git

--- alantang1977/config.py
+++ alantang1977/config.py
@@ -1,9 +1,4 @@
-# 配置文件，包含直播源URL、黑名单URL、公告信息、EPG URL、测速超时时间和线程池最大工作线程数
-
-# 优先使用的IP版本，这里设置为ipv6
-ip_version_priority = "ipv6"
-
-# 直播源URL列表
+# 直播源URL列表（建议去重）
 source_urls = [
     "https://yk95.yymmiptv.top",
     "https://cnb.cool/junchao.tang/llive/-/git/raw/main/中国IPTV",
@@ -14,12 +9,7 @@ source_urls = [
     "http://47.120.41.246:8899/zb.txt",
     "http://live.nctv.top/x.txt",
     "https://gh.tryxd.cn/https://raw.githubusercontent.com/alantang1977/iptv_api/refs/heads/main/output/live_ipv4.m3u",
-    "https://live.izbds.com/tv/iptv4.txt",
-    "http://47.120.41.246:8899/zb.txt",
-    "http://rihou.cc:555/gggg.nzk",
-    "http://api.mytv666.top/lives/free.php?type=txt",
     "http://1.94.31.214/live/livelite.txt",
-    "https://web.banye.tech:7777/tvbus/yogurtTv.txt",
     "https://cnb.cool/junchao.tang/llive/-/git/raw/main/go.txt", 
     "https://smart.pendy.dpdns.org/m3u/Smart.m3u",
     "https://cnb.cool/junchao.tang/llive/-/git/raw/main/咪咕直播",
@@ -28,7 +18,6 @@ source_urls = [
     "https://gh.tryxd.cn/https://raw.githubusercontent.com/peterHchina/iptv/refs/heads/main/IPTV-V4.m3u",
     "https://gh.tryxd.cn/https://raw.githubusercontent.com/peterHchina/iptv/refs/heads/main/IPTV-V6.m3u",
     "http://lisha521.dynv6.net.fh4u.org/tv.txt",
-    "https://web.banye.tech:7777/tv/hlj.txt",
     "https://iptv.catvod.com/tv.m3u",
     "https://live.zbds.top/tv/iptv4.txt",
     "https://gitee.com/xxy002/zhiboyuan/raw/master/dsy",
@@ -52,43 +41,27 @@ source_urls = [
     "https://gh.tryxd.cn/https://raw.githubusercontent.com/cymz6/AutoIPTV-Hotel/main/lives.txt",
     "https://gh.tryxd.cn/https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
     "https://fm1077.serv00.net/SmartTV.m3u",
-    "https://gh.tryxd.cn/https://raw.githubusercontent.com/YanG-1989/m3u/main/Gather.m3u",    
     "https://gh.tryxd.cn/https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.m3u",
     "https://live.zhoujie218.top/tv/iptv4.txt"
 ]
 
-# 直播源黑名单URL列表，去除了重复项
+# 黑名单，禁止采集的部分URL片段（去重，不要覆盖为空）
 url_blacklist = [
-    "epg.pw/stream/",
-    "103.40.13.71:12390",
-    "[2409:8087:1a01:df::4077]/PLTV/",
+    "epg.pw/stream/", "103.40.13.71:12390", "[2409:8087:1a01:df::4077]/PLTV/",
     "http://[2409:8087:1a01:df::7005]:80/ottrrs.hl.chinamobile.com/PLTV/88888888/224/3221226419/index.m3u8",
     "http://[2409:8087:5e00:24::1e]:6060/000000001000/1000000006000233001/1.m3u8",
-    "8.210.140.75:68",
-    "154.12.50.54",
-    "yinhe.live_hls.zte.com",
-    "8.137.59.151",
-    "[2409:8087:7000:20:1000::22]:6060",
-    "histar.zapi.us.kg",
-    "www.tfiplaytv.vip",
-    "dp.sxtv.top",
-    "111.230.30.193",
-    "148.135.93.213:81",
-    "live.goodiptv.club",
-    "iptv.luas.edu.cn",
-    "[2409:8087:2001:20:2800:0:df6e:eb22]:80",
+    "8.210.140.75:68", "154.12.50.54", "yinhe.live_hls.zte.com", "8.137.59.151",
+    "[2409:8087:7000:20:1000::22]:6060", "histar.zapi.us.kg", "www.tfiplaytv.vip",
+    "dp.sxtv.top", "111.230.30.193", "148.135.93.213:81", "live.goodiptv.club",
+    "iptv.luas.edu.cn", "[2409:8087:2001:20:2800:0:df6e:eb22]:80",
     "[2409:8087:2001:20:2800:0:df6e:eb23]:80",
     "[2409:8087:2001:20:2800:0:df6e:eb1d]/ott.mobaibox.com/",
-    "[2409:8087:2001:20:2800:0:df6e:eb1d]:80",
-    "[2409:8087:2001:20:2800:0:df6e:eb24]",
-    "2409:8087:2001:20:2800:0:df6e:eb25]:80",
-    "stream1.freetv.fun",
-    "chinamobile",
-    "gaoma",
-    "[2409:8087:2001:20:2800:0:df6e:eb27]"
+    "[2409:8087:2001:20:2800:0:df6e:eb1d]:80", "[2409:8087:2001:20:2800:0:df6e:eb24]",
+    "2409:8087:2001:20:2800:0:df6e:eb25]:80", "stream1.freetv.fun", "chinamobile",
+    "gaoma", "[2409:8087:2001:20:2800:0:df6e:eb27]"
 ]
 
-# 公告信息
+# 公告频道
 announcements = [
     {
         "channel": "更新日期",
@@ -110,3 +83,4 @@ epg_urls = [
     "https://epg.pw/xmltv/epg_HK.xml",
     "https://epg.pw/xmltv/epg_TW.xml"
 ]
+ip_version_priority = "ipv4"
